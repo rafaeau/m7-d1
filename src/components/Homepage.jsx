bcecfbcb0843a45fa9f0c49415ce9f9230d6b28c
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-24 14:49:44 +0100

    Search engine working

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { FormControl } from 'react-bootstrap';
 
 export default class Homepage extends Component {
     state = {
         jobs: [],
+        search: ""
     }
     render() {
         return (
             <>
+                <FormControl
+                    placeholder="Search jobs"
+                    value={this.state.value}
+                    onChange={(e) => this.setState({search: e.currentTarget.value.toLowerCase()})}
+                    />
+
                 <ul>
-                    {this.state.jobs.data && this.state.jobs.data.map(job =>
+                    {this.state.jobs.data && this.state.jobs.data.filter(job => job.title.toLowerCase().indexOf(this.state.search) !== -1).map(job =>
                         <li key={job._id}>{job.title} at {job.company_name}</li>)}
                 </ul>
             </>
@@ -16,7 +24,7 @@ export default class Homepage extends Component {
 
     }
     componentDidMount = async () => {
-        const resp = await fetch('https://strive-jobs-api.herokuapp.com/jobs?limit=20&skip=20')
+        const resp = await fetch('https://strive-jobs-api.herokuapp.com/jobs?search=developer&limit=30')
         if (resp.ok) {
             const jobs = await resp.json()
             this.setState({
